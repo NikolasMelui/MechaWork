cb53f6b0a48721756c4d637921cb2209c7fe81a7
nikolasmelui <slak@samaradom.ru>
2020-05-02 23:55:41 +0400

    Create users.module exampe

---
+++ src/api/users/users.module.js
@@ -0,0 +1,19 @@
+// const usersController = require('./users.controller');
+// const usersService = require('./users.service');
+
+// const UsersModule = {
+//   controller: [usersController],
+//   service: [usersService],
+// };
+
+// module.exports = UsersModule;
+
+class UsersModule {
+  constructor() {
+    // super();
+    // this.controller = prepareController([tasksController]);
+    // this.service = [tasksService];
+  }
+}
+
+module.exports = UsersModule;
